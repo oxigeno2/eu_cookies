48c3c8df931eb44dec8d81f0560bfd626150d25e
Roland Graf <oxigen.rg@gmail.com>
2015-10-31 16:20:21 +0100

    Add Czech language

---
+++ languages/Czech/Czech_admin.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2015 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link http://oxigen.mablog.eu/
+ */
+define("LAN_PLUGIN_EUC_STYLE", "Vzhled");
+define("LAN_PLUGIN_EUC_TEXT", "Zobrazený text");
+define("LAN_PLUGIN_EUC_POLICYLINK", "Odkaz na stránky s vysvìtlením EU ");
+define("LAN_PLUGIN_EUC_BTN", "Text na tlaèítku pro odsouhlasení");
+define("LAN_PLUGIN_EUC_POSITION", "Umístìní baneru");
+define("LAN_PLUGIN_EUC_THEME", "Barevné schéma banneru");
+define("LAN_PLUGIN_EUC_OPENEFFECT", "Animace pøi otevøení baneru");
+define("LAN_PLUGIN_EUC_OPENEFFECTDURATION", "Doba efektu v milisekndách");
+define("LAN_PLUGIN_EUC_OPENEFFECTEASING", "Zmírnìní efektu pøi otevøení");
+define("LAN_PLUGIN_EUC_CLOSEEFFECT", "Animace pøi zavøení baneru");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTDURATION", "Doba efektu v milisekndách");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTEASING", "Zmírnìní efektu pøi uzavøení");
+?>
---
+++ languages/Czech/Czech_global.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2015 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link http://oxigen.mablog.eu/
+ */
+define("LAN_PLUGIN_EUC_NAME", "EU cookies");
+define("LAN_PLUGIN_EUC_SUMMARY",  "EU Cookie informuje uživatele, že vaše stránky používá cookies."); 
+define("LAN_PLUGIN_EUC_DESCRIPTION", "EU Cookie informuje uživateleže vaše stránky používají cookies, doplnìk vytvoøen s pomocí skriptu Divas Cookies.");
+
+?>
