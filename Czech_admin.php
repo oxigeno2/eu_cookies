f6d501536faa57848e204a872ebde3bd3d483659
Roland Graf <oxigen.rg@gmail.com>
2017-10-28 15:05:31 +0200

    change file encoding

---
+++ Czech_admin.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2015 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link http://oxigen.mablog.eu/
+ */
+define("LAN_PLUGIN_EUC_STYLE", "Vzhled");
+define("LAN_PLUGIN_EUC_TEXT", "Zobrazený text");
+define("LAN_PLUGIN_EUC_POLICYLINK", "Odkaz na stránky s vysvìtlením EU ");
+define("LAN_PLUGIN_EUC_BTN", "Text na tlačítku pro odsouhlasení");
+define("LAN_PLUGIN_EUC_POSITION", "Umístění baneru");
+define("LAN_PLUGIN_EUC_THEME", "Barevné schéma banneru");
+define("LAN_PLUGIN_EUC_OPENEFFECT", "Animace při otevření baneru");
+define("LAN_PLUGIN_EUC_OPENEFFECTDURATION", "Doba efektu v milisekndách");
+define("LAN_PLUGIN_EUC_OPENEFFECTEASING", "Zmírnění efektu při otevření");
+define("LAN_PLUGIN_EUC_CLOSEEFFECT", "Animace pøi zavření baneru");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTDURATION", "Doba efektu v milisekndách");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTEASING", "Zmírnění efektu při uzavření");
+?>
