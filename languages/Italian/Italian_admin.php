9b8cdd8993f9b552977b67af997801dbf21dd8e8
Roland Graf <oxigen.rg@gmail.com>
2017-10-30 20:37:15 +0100

    Add Italian translation

--- languages/Italian/Italian_admin.php
+++ languages/Italian/Italian_admin.php
@@ -1,1 +1,46 @@
+<?php
 
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2015 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link http://oxigen.mablog.eu/
+ */
+ 
+define("LAN_PLUGIN_EUC_STYLE", "Stile");
+define("LAN_PLUGIN_EUC_TEXT", "Testo per il banner");
+define("LAN_PLUGIN_EUC_POLICYLINK", "Link per estendere le policy dei cookie");
+define("LAN_PLUGIN_EUC_BTN", "Questo é il testo per il bottone di conferma.");
+define("LAN_PLUGIN_EUC_POSITION", "Posizioni  banner");
+define("LAN_PLUGIN_EUC_THEME", "Colore del banner");
+define("LAN_PLUGIN_EUC_OPENEFFECT", "Effetto apertura del banner dei cookie.");
+define("LAN_PLUGIN_EUC_OPENEFFECTDURATION", "durata per effetto di apertura del banner in msec");
+define("LAN_PLUGIN_EUC_OPENEFFECTEASING", "funzionalità apertura banner");
+define("LAN_PLUGIN_EUC_CLOSEEFFECT", "effetto di chiusura");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTDURATION", "durata effetto di chiusura del banner in msec");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTEASING", "funzionalità di chusura del banner");
+define("LAN_PLUGIN_EUC_FADE",  "dissolvenza");
+define("LAN_PLUGIN_EUC_SLIDEDOWN",  "scorri in basso");
+define("LAN_PLUGIN_EUC_SLIDEUP",  "scorri in alto");
+define("LAN_PLUGIN_EUC_SLIDELEFT",  "scorri a sinistra");
+define("LAN_PLUGIN_EUC_SLIDERIGHT",  "scorri a destra");
+define("LAN_PLUGIN_EUC_TOP",  "Alto");
+define("LAN_PLUGIN_EUC_BOTTOM",  "Basso");
+define("LAN_PLUGIN_EUC_DARK",  "Scuro");
+define("LAN_PLUGIN_EUC_LIGHT",  "Chiaro");
+define("LAN_PLUGIN_EUC_SWING",  "Oscillante");
+define("LAN_PLUGIN_EUC_LINEAR",  "Lineare");
+
+// Campo di aiuto
+define ("LAN_PLUGIN_EUC_HELP_TEXT", "Spiegare l'uso dei cookie, quale scopo");
+define ("LAN_PLUGIN_EUC_HELP_POLICYLINK", "Descrizione del pulsante, collegamento al sito ufficiale con spiegazione");
+define ("LAN_PLUGIN_EUC_HELP_BTN", "Descrizione per accettare l'utilizzo dei cookie");
+
+// Amministrazione di aiuto
+define ("LAN_PLUGIN_EUC_HELP_ADMIN_01", "Spiegazione");
+define ("LAN_PLUGIN_EUC_HELP_ADMIN_02", "I cookie sono file di testo corti che un server inserisce nel computer dell'utente quando visualizza siti web. Cookies raccoglie informazioni sul comportamento dell'utente su Internet, serve per personalizzare i contenuti, monitorare, misurare e identificare.");
+define ("LAN_PLUGIN_EUC_HELP_ADMIN_03", "Chi richiede il consenso all'uso dei cookie?");
+define ("LAN_PLUGIN_EUC_HELP_ADMIN_04", "L'utente deve essere fornito di informazioni chiare, comprensibili e complete sul trattamento dei dati, in particolare il suo scopo. Non è sufficiente che le informazioni siano a disposizione dell'utente in qualche sito dove avrebbe per cercarlo stesso. Queste informazioni devono essere fornite direttamente.");
+?>
---
+++ languages/Italian/Italian_global.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2015 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link http://oxigen.mablog.eu/
+ */
+define("LAN_PLUGIN_EUC_NAME", "EU cookies");
+define("LAN_PLUGIN_EUC_SUMMARY",  "EU Cookie informa gli utenti che il tuo utilizza i cookies."); 
+define("LAN_PLUGIN_EUC_DESCRIPTION", "EU Cookie informa gli utenti che il tuo sito web utilizza i cookie,  plugin creato sulla base dello script Divas Cookies");
+
+?>
